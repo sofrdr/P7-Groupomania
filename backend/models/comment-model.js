6395e9d2e78c9ce23872226363e3c0b400710def
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-08-23 12:21:29 +0200

    Ajout comment-controller

--- backend/models/comment-model.js
+++ backend/models/comment-model.js
@@ -11,3 +11,21 @@ FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
 
 createTable('comments', structureComment);
 joinTable('comments', 'posts', 'post_id', 'id');
+
+
+function getComments(id){
+    const comments = db.prepare(`SELECT * FROM comments WHERE post_id = ?`)
+    .all(id);
+    return comments
+}
+
+function createComment(author, message, post_id) {
+    newComment = db.prepare(`INSERT INTO comments (author, message, date, post_id) VALUES (@author, @message, datetime('now', 'localtime'), @post_id)`)
+    .run({        
+        author: author,
+        message: message, 
+        post_id: post_id      
+    })
+}
+
+module.exports = {getComments, createComment};
