0b6143717ae4d16a34e2281622754177448f5d4f
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-09-02 11:58:39 +0200

    Ajout des fonctions SignIn + SignUp sans gestion des erreurs

--- client/src/components/connection/SignIn.js
+++ client/src/components/connection/SignIn.js
@@ -1,8 +1,53 @@
-import React from "react"
+import React, {useState} from "react"
 
 const SignIn = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const dataLogin = {
+        email,
+        password
+    }
+
+    
+    const handleSignIn = (e) => {
+        
+        e.preventDefault();
+        fetch("http://localhost:3000/api/auth/login", {
+            method: 'POST',
+            body: JSON.stringify(dataLogin),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .catch(err => console.error(err)) 
+    }
     return(
-        "Se connecter"
+        <div>
+            <form onSubmit={handleSignIn}>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email" 
+                id="email" 
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                />
+
+                <label htmlFor="password">Mot de passe</label>
+                <input 
+                type="password" 
+                id="password" 
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                />
+                <input type="submit" value="Se connecter"/>
+            </form>
+        </div>
     )
 }
 
--- client/src/components/connection/SignUp.js
+++ client/src/components/connection/SignUp.js
@@ -1,8 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
 
 const SignUp = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const dataSignUp = {
+        email,
+        password
+    }
+
+    const handleSignUp = (e) => {
+        
+        e.preventDefault();
+        fetch("http://localhost:3000/api/auth/signup", {
+            method: 'POST',
+            body: JSON.stringify(dataSignUp),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .catch(err => console.error(err)) 
+    }
+
     return(
-        "Créer un compte"
+        <div className="signup-form-container">
+            <form id="signup-form" onSubmit={handleSignUp}>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email" 
+                id="email" 
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                />
+
+                <label htmlFor="password">Mot de passe</label>
+                <input 
+                type="password" 
+                id="password" 
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                />
+                <input type="submit" value="Créer un compte"/>
+            </form>
+        </div>
     )
 }
 
