7d953babc97d6b78607b201261a368d41023fd0a
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-09-20 19:44:30 +0200

    Création librairie font awesome

---
+++ client/src/services/fontawesome.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faHeart, faImage, faComment } from '@fortawesome/free-regular-svg-icons';
+import { faEllipsis, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+    faHeart,
+    faImage,
+    faEllipsis,
+    faRightFromBracket,
+    faComment
+)
