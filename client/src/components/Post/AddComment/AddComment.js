7a70caf312d740fa3e51e8cdb18237820c2554ba
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-10-18 11:31:42 +0200

    Ajout condition AddComment.js, le commentaire est ajouté si le message n'est pas vide

--- client/src/components/Post/AddComment/AddComment.js
+++ client/src/components/Post/AddComment/AddComment.js
@@ -7,39 +7,42 @@ const AddComment = (props) => {
     const [message, setmessage] = useState("")
 
     const postId = props.id
-    const {createComment, toggleAddComment} = props
+    const { createComment, toggleAddComment } = props
 
     const handlemessage = (e) => {
         setmessage(e.target.value)
     }
-    
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
-            const data = await addComment({message}, postId)          
+        try {
+            const data = await addComment({ message }, postId)
             const newComment = data.newComment
-            createComment(postId, newComment)
-            toggleAddComment()
-        }catch(err){
+            if (message !== "") {
+                createComment(postId, newComment)
+                toggleAddComment()
+            }
+
+        } catch (err) {
             console.log(err)
         }
     }
 
-    return(
+    return (
         <div className="comment-container ">
             <form onSubmit={handleSubmit}>
-                <input   
-                className="new-comment-content"             
-                type="text"
-                id="message"
-                name="message"
-                placeholder="Laissez un commentaire ..."
-                value={message}
-                onChange={handlemessage}
+                <input
+                    className="new-comment-content"
+                    type="text"
+                    id="message"
+                    name="message"
+                    placeholder="Laissez un commentaire ..."
+                    value={message}
+                    onChange={handlemessage}
                 />
             </form>
-            
+
         </div>
     )
 
